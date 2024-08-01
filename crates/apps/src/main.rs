928d8a9d9d597b972b255bd3a1b46f28b8bbdfd5
Scotte Zinn <scotte@zinn.ca>
2024-08-01 09:19:11 -0400

    feat: Add core and api crates

--- crates/apps/src/main.rs
+++ crates/apps/src/main.rs
@@ -1,3 +1,5 @@
+use arch_core::create_service;
+
 use anyhow::Result;
 
 mod logging;
@@ -6,7 +8,9 @@ mod logging;
 async fn main() -> Result<()> {
     logging::init_logging()?;
 
+    let arch_service = create_service();
     tracing::info!("Hello, world!");
+    tracing::info!("Healthy={}", arch_service.health_service.is_healthy().await);
 
     Ok(())
 }
