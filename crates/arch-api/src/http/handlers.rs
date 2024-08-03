e67ba50fdfbb95fd05bf159b77b4319edf12c0eb
Scotte Zinn <scotte@zinn.ca>
2024-08-03 10:40:08 -0400

    feat: Refactor http handler

---
+++ crates/arch-api/src/http/handlers.rs
@@ -0,0 +1,43 @@
+use crate::ApiError;
+use std::{net::SocketAddr, sync::Arc, time::Duration};
+
+use arch_core::ArchService;
+use axum::{extract::Request, routing::get, Router};
+use hyper::body::Incoming;
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpStream;
+use tokio_graceful_shutdown::SubsystemHandle;
+use tower::Service;
+use tower_http::timeout::TimeoutLayer;
+
+use super::health;
+
+
+pub fn get_routes(arch_service: Arc<ArchService>) -> Router<()> {
+    axum::Router::new()
+        .route("/api/v1/health", get(health::health))
+        .with_state(arch_service)
+        .layer(TimeoutLayer::new(Duration::from_secs(2)))
+}
+
+pub async fn handle(socket: TcpStream, remote_addr: SocketAddr, tower_service: Router<()>, subsys: SubsystemHandle) -> Result<(), ApiError> {
+    let socket = TokioIo::new(socket);
+    let hyper_service = hyper::service::service_fn(move |request: Request<Incoming>| tower_service.clone().call(request));
+    let conn = hyper::server::conn::http1::Builder::new().serve_connection(socket, hyper_service);
+    let mut conn = std::pin::pin!(conn);
+
+    tokio::select! {
+        result = conn.as_mut() => {
+            if let Err(err) = result {
+                tracing::warn!("Failed to serve connection{}: {:#}", remote_addr, err);
+            }
+        }
+
+        _ = subsys.on_shutdown_requested() => {
+            tracing::debug!("signal received, starting graceful shutdown");
+        }
+    }
+
+    tracing::debug!("Connection {} closed", remote_addr);
+    Ok(())
+}
