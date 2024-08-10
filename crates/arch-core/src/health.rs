179fa9ff6f59629cc1fec488a2ca97323eac95a5
Scotte Zinn <scotte@zinn.ca>
2024-08-09 20:22:00 -0400

    feat: Refactor ArchService use (#28)

--- crates/arch-core/src/health.rs
+++ crates/arch-core/src/health.rs
@@ -1,10 +1,18 @@
 use async_trait::async_trait;
 
-use crate::HealthService;
+#[async_trait]
+pub trait HealthService: Send + Sync {
+    async fn is_healthy(&self) -> bool;
+}
 
 #[derive(Clone)]
 pub(crate) struct HealthServiceImpl {}
 
+impl HealthServiceImpl {
+    pub(crate) fn new() -> Self {
+        Self {}
+    }
+}
 #[async_trait]
 impl HealthService for HealthServiceImpl {
     #[tracing::instrument(level = "trace", skip(self))]
