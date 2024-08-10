c841ae2aab6f924578d145a23324595f374356a9
Scotte Zinn <scotte@zinn.ca>
2024-08-10 16:00:03 -0400

    feat: Introduce ArcBox macro (#29)

--- crates/arch-core/src/lib.rs
+++ crates/arch-core/src/lib.rs
@@ -1,17 +1,16 @@
-use std::sync::Arc;
-
+use arch_utils::{arcbox, arcbox::ArcBox};
 use health::HealthServiceImpl;
 
 mod health;
 pub use health::HealthService;
 
 pub struct ArchService {
-    pub health_service: Arc<Box<dyn HealthService>>,
+    pub health_service: ArcBox<dyn HealthService>,
 }
 
 pub fn create_service() -> ArchService {
     let health_service = HealthServiceImpl::new();
-    let health_service = Arc::new(Box::new(health_service) as Box<dyn HealthService>);
+    let health_service: ArcBox<dyn HealthService> = arcbox!(health_service);
 
     ArchService { health_service }
 }
