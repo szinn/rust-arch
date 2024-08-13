23a4d133eac5275b395654fb00d92aca5d4b6421
Scotte Zinn <scotte@zinn.ca>
2024-08-13 16:09:18 -0400

    feat: Switch to db agnostic migrations

--- crates/arch-db-pg/src/lib.rs
+++ crates/arch-db-pg/src/lib.rs
@@ -1,22 +1,14 @@
 use std::sync::Arc;
 
+use migration::apply_migrations;
 use sea_orm::{ConnectOptions, Database, DatabaseConnection};
-pub use sea_orm_migration::prelude::*;
-
-pub mod error;
-pub use error::*;
 use tracing_log::log;
 
-mod m20220101_000001_create_table;
+pub mod entities;
+pub mod error;
+pub mod migration;
 
-pub struct Migrator;
-
-#[async_trait::async_trait]
-impl MigratorTrait for Migrator {
-    fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20220101_000001_create_table::Migration)]
-    }
-}
+pub use error::*;
 
 pub struct DatabaseRepository {
     pub database: DatabaseConnection,
@@ -31,14 +23,10 @@ pub async fn create_database_connection(url: &str) -> Result<Arc<DatabaseReposit
         .sqlx_logging_level(log::LevelFilter::Info);
 
     let database = Database::connect(opt).await.map_err(handle_dberr)?;
-    Migrator::up(&database, None).await?;
+    apply_migrations(&database).await?;
 
     let database = Arc::new(DatabaseRepository { database });
     tracing::debug!("...connected to database");
 
     Ok(database)
-}
-
-pub async fn run_migration_cli() {
-    cli::run_cli(Migrator).await
 }
